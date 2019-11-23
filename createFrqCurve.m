0a590241b559c8160fc103863b8e1fdedd792cc7
XDGFX <callum.morrison@mac.com>
2019-11-23 19:37:13 +0000

    stuff

--- createFrqCurve.m
+++ createFrqCurve.m
@@ -20,23 +20,24 @@ function [frq] = createFrqCurve(v, dist)
 
 % First assume peak frequency is not reached
 % Triangular profile
-xMax = round(2 * (dist / v.m)^0.5 * 2 * v.SCF);
+%xMax = round(2 * (dist / v.m)^0.5);
+%xMax = dist
 
-if xMax * v.m / 2 > v.frq
-    % Peak frequency is surpassed
-    % Re-calculate with trapezoidal
-    disp("too high")
-    
-    % Convert distance using SCF
-    xMax = round((dist^2 / (2*v.frq) + dist * v.frq * v.m) * v.SCF / 800000);
-    
-end
+% if xMax * v.m / 2 > v.frq
+%     % Peak frequency is surpassed
+%     % Re-calculate with trapezoidal
+%     disp("too high")
+%     
+%     % Convert distance using SCF
+%     xMax = round((dist^2 / (2*v.frq) + dist * v.frq * v.m));
+%     
+% end
 
 % Create profile x dimensions
-x = 1:xMax;
+x = 1:dist;
 
-h1 = 1:round(xMax / 2);
-h2 = round(xMax / 2) + 1:xMax;
+h1 = 1:round(dist / 2);
+h2 = round(dist / 2) + 1:dist;
 
 % Create triangle profile
 frq(h1) = v.m .* x(h1);
@@ -44,5 +45,8 @@ frq(h2) = flip(v.m .* x(1:length(h2)));
 
 % Limit values above v.frq
 frq(frq > v.frq) = v.frq;
+
+% Minimum floor value
+frq(frq < 100) = 100;
 
 plot(x, frq)
--- goToPos.m
+++ goToPos.m
@@ -1,15 +1,58 @@
 function [v] = goToPos(a, p, v, position)
 % Sends gantry to pos = [x, y] as fast as possible
 
-pos.x = position(1);
-pos.y = position(2);
+target.x = position(1);
+target.y = position(2);
 
 % Read current position
 v = getPos(a, v, p);
 
 % Calculate distance required to travel
-dist.x = pos.x - v.pos.x;
-dist.y = pos.y - v.pos.y;
+dist.x = abs(target.x - v.pos.x);
+dist.y = abs(target.y - v.pos.y);
+
+% Determine start positions
+start.x = v.pos.x;
+start.y = v.pos.y;
+
+% Determine required direction
+for d = ["x", "y"]
+    if target.(d) > v.pos.(d)
+        % Set direction
+        v.direction.(d) = 1;
+        writeDigitalPin(a.a, p.direction.(d), v.direction.(d))
+    else
+        % Set direction
+        v.direction.(d) = 0;
+        writeDigitalPin(a.a, p.direction.(d), v.direction.(d))
+    end
+end
+
+% Read current position
+v = getPos(a, v, p);
+
+% Enable both axes
+playTone(a.a, p.ramp, 0, 0.1) % Clear playtone pin
+writeDigitalPin(a.a, p.enable.x, 0)
+writeDigitalPin(a.a, p.enable.y, 0)
+
+frq = 100;
+while abs(v.pos.x - start.x) < dist.x / 2
+    v = getPos(a, v, p);
+    disp(v.pos.x)
+    %disp(frq)
+    playTone(a.a, p.ramp, frq, 0.1)
+    frq = frq + v.m;
+end
+
+while abs(v.pos.x - start.x) < dist.x
+    v = getPos(a, v, p);
+    distLeft.x = abs(target.x - v.pos.x);
+    disp(v.pos.x)
+    %disp(frq)
+    playTone(a.a, p.ramp, frq, 0.1)
+    frq = frq * distLeft.x * 2 / dist.x;
+end
 
 % Assign shortest distance variable to 's' and longest to 'l'
 if dist.x < dist.y
@@ -19,10 +62,6 @@ else
     s = "y";
     l = "x";
 end
-
-
-
-dist.(s);
 
 end
 
